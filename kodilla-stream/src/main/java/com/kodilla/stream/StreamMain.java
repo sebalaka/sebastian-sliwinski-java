95ae584e7a7679c717062216541cc8cd0881bcc7
sebalaka <sebalaka@gmail.com>
2020-09-20 13:03:00 +0200

    7.2 end immutable class

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -2,6 +2,7 @@ package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
 import com.kodilla.stream.beautifier.PoemDecorator;
+import com.kodilla.stream.iterate.NumbersGenerator;
 
 public class StreamMain {
     public static void main(String[] args) {
@@ -24,5 +25,8 @@ public class StreamMain {
         poemBeautifier.beautify("Weed Power",(String::toUpperCase));
         poemBeautifier.beautify("Weed Power",(String::toLowerCase));
         poemBeautifier.beautify("        Weed Power",(String::trim));
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
