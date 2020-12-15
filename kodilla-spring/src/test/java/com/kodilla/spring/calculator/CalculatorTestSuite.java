5df703916b9e54456d3a1391f912e88797cea295
sebalaka <sebalaka@gmail.com>
2020-12-15 19:33:18 +0100

    zadanie 14.2

--- kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -1,4 +1,59 @@
 package com.kodilla.spring.calculator;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class CalculatorTestSuite {
+
+    @Test
+    public void testCalculationsAdd() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = context.getBean(Calculator.class);
+        //When
+        double result = calculator.add(2,2);
+        //Then
+        Assert.assertEquals(4.00, result, 0.01);
+    }
+
+    @Test
+    public void testCalculationsSub() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = context.getBean(Calculator.class);
+        //When
+        double result = calculator.sub(2,2);
+        //Then
+        Assert.assertEquals(0, result, 0.01);
+    }
+
+    @Test
+    public void testCalculationsMul() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = context.getBean(Calculator.class);
+        //When
+        double result = calculator.mul(2,2);
+        //Then
+        Assert.assertEquals(4, result, 0.01);
+    }
+
+    @Test
+    public void testCalcuationsDiv() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = context.getBean(Calculator.class);
+        //When
+        double result = calculator.div(2,2);
+        //Then
+        Assert.assertEquals(1, result, 0.01);
+    }
 }
