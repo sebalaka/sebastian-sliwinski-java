43ae9d797eb4a8e957fed9211e63dafad40099de
sebalaka <sebalaka@gmail.com>
2020-12-29 18:10:27 +0100

    15.5 sample@

---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/builder/bigmac/Bigmac.java
@@ -0,0 +1,14 @@
+package com.kodilla.patterns.builder.bigmac;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Bigmac {
+    private final String bun;
+    private final int burgers;
+    private final String sauce;
+    private  List<String> ingredients = new ArrayList<>();
+
+
+}
